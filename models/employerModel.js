5466ac43b7e7edfad32c98f3c2a79b10d0497528
atheersnm@gmail.com <atheersnm@gmail.com>
2024-08-01 18:53:08 +0300

    define request crud for employers and find by id

---
+++ models/employerModel.js
@@ -0,0 +1,96 @@
+const db = require("../config/db");
+
+const Employer = {
+  create: (employer, callback) => {
+    const query =
+      "INSERT INTO Employers (company_name, company_description, contact_email, employee_seeker_id) VALUES (?, ?, ?, ?)";
+    console.log("Executing query:", query, employer);
+    db.query(
+      query,
+      [
+        employer.company_name,
+        employer.company_description,
+        employer.contact_email,
+        employer.employee_seeker_id,
+      ],
+      (err, result) => {
+        if (err) {
+          console.error("Error executing query:", err);
+          callback(err, null);
+          return;
+        }
+        console.log("Query executed successfully:", result);
+        callback(null, result);
+      }
+    );
+  },
+
+  findAll: (callback) => {
+    const query = "SELECT * FROM Employers";
+    console.log("Executing query:", query);
+    db.query(query, (err, results) => {
+      if (err) {
+        console.error("Error executing query:", err);
+        callback(err, null);
+        return;
+      }
+      console.log("Query executed successfully:", results);
+      callback(null, results);
+    });
+  },
+
+  findById: (id, callback) => {
+    const query = "SELECT * FROM Employers WHERE employer_id = ?";
+    console.log("Executing query:", query, id);
+    db.query(query, [id], (err, result) => {
+      if (err) {
+        console.error("Error executing query:", err);
+        callback(err, null);
+        return;
+      }
+      console.log("Query executed successfully:", result);
+      callback(null, result);
+    });
+  },
+
+  update: (id, employer, callback) => {
+    const query =
+      "UPDATE Employers SET company_name = ?, company_description = ?, contact_email = ?, employee_seeker_id = ? WHERE employer_id = ?";
+    console.log("Executing query:", query, employer, id);
+    db.query(
+      query,
+      [
+        employer.company_name,
+        employer.company_description,
+        employer.contact_email,
+        employer.employee_seeker_id,
+        id,
+      ],
+      (err, result) => {
+        if (err) {
+          console.error("Error executing query:", err);
+          callback(err, null);
+          return;
+        }
+        console.log("Query executed successfully:", result);
+        callback(null, result);
+      }
+    );
+  },
+
+  delete: (id, callback) => {
+    const query = "DELETE FROM Employers WHERE employer_id = ?";
+    console.log("Executing query:", query, id);
+    db.query(query, [id], (err, result) => {
+      if (err) {
+        console.error("Error executing query:", err);
+        callback(err, null);
+        return;
+      }
+      console.log("Query executed successfully:", result);
+      callback(null, result);
+    });
+  },
+};
+
+module.exports = Employer;
